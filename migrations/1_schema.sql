c541aaca109054e4eca91ee0190f347aba0569a6
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-08 18:38:14 +0000

    generate tables that will contain essential info

---
+++ migrations/1_schema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE users (
+  id SERIAL PRIME KEY NOT NULL, 
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+)
+
+CREATE TABLE properties (
+  id SERIAL PRIME KEY,
+  ownder_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  thumbnail_photo_url VARHAR(255) NOT NULL,
+  cover_photo_url VARCHAR(255) NOT NULL,
+  cost_per_night SMALLINT NOT NULL DEFAULT 0,
+  parking_spaces SMALLINT NOT NULL DEFAULT 0,
+  umber_of_bathrooms SMALLINT NOT NULL DEFAULT 0,
+  number_of_bedrooms SMALLINT NOT NULL DEFAULT 0,
+  
+  country VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
+  province VARCHAR(255) NOT NULL,
+  post_code VARCHAR(255) NOT NULL,
+  
+  active BOOLEAN NOT NULL DEFAULT TRUE
+)
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night SMALLINT,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+)
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+)
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+)
