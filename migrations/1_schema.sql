08d716e16aedf75ffa57399f20bb7a2a23b45433
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-08 21:57:52 +0000

    provide sample queries

--- migrations/1_schema.sql
+++ migrations/1_schema.sql
@@ -35,20 +35,20 @@ CREATE TABLE properties (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
-CREATE TABLE rates (
-  id SERIAL PRIMARY KEY,
-  start_date DATE,
-  end_date DATE,
-  cost_per_night SMALLINT,
-  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
-);
-
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  cost_per_night SMALLINT,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
 
 CREATE TABLE guest_reviews (
