f7c92be0ae7e54dd9791d5df757340de82e58f78
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-10 14:21:24 +0000

    adds ability to create listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,7 +85,7 @@ const getAllReservations = function(guest_id, limit = 10) {
   WHERE guest_id = $1
   LIMIT $2
   `, value)
-    .then(res => res);
+    .then(res => res.rows);
 };
 exports.getAllReservations = getAllReservations;
 
@@ -98,10 +98,64 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
+  const queryParams = [];
+  let condition = ' WHERE ';
+  let connect = '';
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id`;
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    condition += ` LOWER(city) LIKE LOWER ($${queryParams.length}) `;
+    connect = ' AND ';
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`%${options.owner_id}%`);
+    condition += connect;
+    condition += `owner_id = $${queryParams.length} `
+    connect = ' AND ';
+  }
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${100 * options.minimum_price_per_night}`);
+    condition += connect;
+    condition += `cost_per_night > $${queryParams.length} `
+    connect = ' AND ';
+  }
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${100 * options.maximum_price_per_night}`);
+    condition += connect;
+    condition += `cost_per_night < $${queryParams.length} `
+    connect = ' AND ';
+  }
+
+
+
+
+
+
+
+  if (queryParams.length === 0) {
+    condition = '';
+  }
+  queryString += condition;
+  queryString += `
+  GROUP BY properties.id `
+    ;
+  if (options.minimum_rating) {
+    queryParams.push(Number(options.minimum_rating));
+    condition += ` HAVING avg(property_reviews.rating) > $${queryParams.length} `
+    queryString += condition;
+  }
+  queryParams.push(limit);
+  queryString += ` ORDER BY cost_per_night
+  Limit $${queryParams.length}`;
+
+  console.log(queryString, queryParams);
+
+  return pool.query(queryString, queryParams)
     .then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
@@ -113,9 +167,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  let queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms];
+
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *
+  `, queryParams);
 }
 exports.addProperty = addProperty;
